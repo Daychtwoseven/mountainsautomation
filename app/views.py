b516d365e75605824cbebd354d291af2ee52c5ee
Richardren Escol <chad>
2022-12-14 13:05:35 +0800

    c

--- app/views.py
+++ app/views.py
@@ -114,7 +114,7 @@ def index_page(request, action=None):
                 url_func = [url_1, url_2, url_3, url_4, url_5, url_6, url_7, url_8, url_9, url_10, url_11, url_12,
                             url_13,
                             url_14, url_15, url_16, url_17, url_18, url_19, url_20, url_21, url_22, url_23, url_24,
-                            url_25, url_26, url_27, url_28, url_29, url_30, url_31, url_32, url_33, url_34, url_36]
+                            url_25, url_26, url_27, url_28, url_29, url_30, url_31, url_32, url_33, url_34, url_35]
 
                 counter = 0
                 success = 0
